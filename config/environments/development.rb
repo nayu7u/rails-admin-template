8c3b247fb4e4a5794d8c62ea2ddbc72cc47b59ef
nayu <19748682+nayu7u@users.noreply.github.com>
2024-04-29 18:01:26 +0900

    bundle install better_errors

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,8 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  if Rails.env.development?
+    BetterErrors::Middleware.allow_ip! "0.0.0.0/0"
+  end
 end
